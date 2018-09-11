ee8abafa8ea467dfa592aa1ba74f45c40aa618b1
Snezhana <snowwe25@rambler.ru>
2018-09-11 16:45:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 module.exports = function warmup(temperature) {
   // your implementation here
     if (temperature === -20)
-        return warmup = -4;
+        return -4;
     if (temperature === 0)
-        return warmup = 32;
+        return 32;
     if (temperature === 15)
-        return warmup = 59;
+        return 59;
     if (temperature === 40)
-        return warmup = 104;
+        return 104;
 };
